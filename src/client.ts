883b083c1a9876d82a70ad3c43f1ededb511653a
Casey Pazera <ParkourPunk@googlemail.com>
2022-05-31 08:10:51 +0100

    removed  non react native libaries and cleaned some code

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,11 @@
-const { format: fmt } = require('util');
-const { coins } = require('@cosmjs/amino');
-const { sortedJsonStringify } = require('@cosmjs/amino/build/signdoc');
-const { fromBase64 } = require('@cosmjs/encoding');
-const memoize = require('lodash.memoize');
-require('dotenv').config();
-const base58 = require('bs58');
-const sovrin = require('sovrin-did');
-const { Secp256k1HdWallet, serializeSignDoc } = require('@cosmjs/amino');
-const {
-  EnglishMnemonic,
-  pathToString,
-  stringToPath,
-  sha256,
-} = require('@cosmjs/crypto');
-const { toBase64, Bech32 } = require('@cosmjs/encoding');
+import { sortedJsonStringify } from '@cosmjs/amino/build/signdoc';
+import { fromBase64 } from '@cosmjs/encoding';
+import memoize from 'lodash.memoize';
+import base58 from 'bs58';
+var sovrin = require('sovrin-did');
+import { Secp256k1HdWallet, serializeSignDoc, coins } from '@cosmjs/amino';
+import { pathToString, sha256 } from '@cosmjs/crypto';
+import { toBase64, Bech32 } from '@cosmjs/encoding';
 import { create } from 'apisauce';
 
 let defaultCellnodeUrl = 'https://cellnode-pandora.ixo.world';
@@ -270,13 +262,11 @@ export function makeClient(
 
     balances: async (accountType: any, denom: any) =>
       await bcFetchGet(
-        fmt(
-          '/cosmos/bank/v1beta1/balances/%s' + (denom ? '/%s' : ''),
+        `/cosmos/bank/v1beta1/balances/${
           (
             await getSignerAccount(accountType)
-          ).address,
-          denom || ''
-        )
+          ).address
+        }` + (denom ? `/${denom}` : '')
       ),
 
     register: (pubKey?: any) => {
@@ -406,7 +396,7 @@ export function makeClient(
           tplName: 'submit_claim',
           data: {
             '@context':
-              'https://schema.ixo.foundation/claims/53690e7d550278dbe228ddf35e0ba72b2666cba6', // eslint-disable-line max-len
+              'https://schema.ixo.foundation/claims/53690e7d550278dbe228ddf35e0ba72b2666cba6',
             'claimTemplateId': tplRec.projectDid,
             'type': tplRec.data.page.content.claimInfo.type,
             'issuerId': signer.agent.did,
@@ -448,12 +438,11 @@ export function makeClient(
 
       myDelegations: async () =>
         await bcFetchGet(
-          fmt(
-            '/staking/delegators/%s/delegations',
+          `/staking/delegators/${
             (
               await getSignerAccount('secp')
             ).address
-          )
+          }/delegations`
         ),
 
       pool: () => bcFetchGet('/staking/pool'),
@@ -463,28 +452,20 @@ export function makeClient(
 
       delegatorValidatorRewards: (delegatorAddr: any, validatorAddr: any) =>
         bcFetchGet(
-          fmt(
-            '/distribution/delegators/%s/rewards/%s',
-            delegatorAddr,
-            validatorAddr
-          )
+          `/distribution/delegators/${delegatorAddr}/rewards/${validatorAddr}`
         ),
 
       delegation: (delegatorAddr: any, validatorAddr: any) =>
         bcFetchGet(
-          fmt(
-            '/staking/delegators/%s/delegations/%s',
-            delegatorAddr,
-            validatorAddr
-          )
+          `/staking/delegators/${delegatorAddr}/delegations/${validatorAddr}`
         ),
 
       delegatorDelegations: (delegatorAddr: any) =>
-        bcFetchGet(fmt('/staking/delegators/%s/delegations', delegatorAddr)),
+        bcFetchGet(`/staking/delegators/${delegatorAddr}/delegations`),
 
       delegatorUnbondingDelegations: (delegatorAddr: any) =>
         bcFetchGet(
-          fmt('/staking/delegators/%s/unbonding_delegations', delegatorAddr)
+          `/staking/delegators/${delegatorAddr}/unbonding_delegations`
         ),
 
       delegatorRewards: (delegatorAddr: any) =>
@@ -668,7 +649,7 @@ export async function getTemplate(tplRecOrDid: string): Promise<any> {
       ),
       decodedTplContent = String.fromCharCode.apply(
         null,
-        fromBase64(rawTplContent)
+        Array.from(fromBase64(rawTplContent))
       ),
       parsedTplContent = JSON.parse(decodedTplContent);
 
@@ -1008,12 +989,15 @@ export async function makeWallet(src: any, didPrefix = 'did:ixo:') {
   let secp: any, agent: any;
 
   if (typeof src === 'object') {
-    ({ secp, agent } = fromSerializableWallet(src));
+    // TODO CLASS
+    // ({ secp, agent } = fromSerializableWallet(src));
   } else {
-    secp = await (src
-      ? Secp256k1HdWallet.fromMnemonic(src, { prefix: 'ixo' })
-      : Secp256k1HdWallet.generate(12, { prefix: 'ixo' }));
-    // See note [1]
+    if (src) {
+      secp = await Secp256k1HdWallet.fromMnemonic(src, { prefix: 'ixo' });
+    } else {
+      secp = await Secp256k1HdWallet.generate(12, { prefix: 'ixo' });
+      // See note [1]
+    }
 
     agent = await makeAgentWallet(secp.mnemonic, undefined, didPrefix);
   }
@@ -1040,24 +1024,24 @@ export function toSerializableWallet(w: { secp: any; agent: any }): any {
     },
   };
 }
+// DISABLED FOR THIS VERSION
+// export function fromSerializableWallet(s: any) {
+//   return {
+//     secp: new Secp256k1HdWallet(
+//       s.secp.mnemonic && new EnglishMnemonic(s.secp.mnemonic),
 
-export function fromSerializableWallet(s: any) {
-  return {
-    secp: new Secp256k1HdWallet(
-      s.secp.mnemonic && new EnglishMnemonic(s.secp.mnemonic),
+//       {
+//         seed: Uint8Array.from(base58.decode(s.secp.seed)),
+//         prefix: s.secp.accounts[0].prefix,
+//         hdPaths: s.secp.accounts.map((a: { hdPath: any }) =>
+//           stringToPath(a.hdPath)
+//         ),
+//       }
+//     ),
 
-      {
-        seed: Uint8Array.from(base58.decode(s.secp.seed)),
-        prefix: s.secp.accounts[0].prefix,
-        hdPaths: s.secp.accounts.map((a: { hdPath: any }) =>
-          stringToPath(a.hdPath)
-        ),
-      }
-    ),
-
-    agent: makeAgentWallet(s.agent.mnemonic, s.agent.didDoc, s.agent.didPrefix),
-  };
-}
+//     agent: makeAgentWallet(s.agent.mnemonic, s.agent.didDoc, s.agent.didPrefix),
+//   };
+// }
 
 /* @returns OfflineAminoSigner: https://github.com/cosmos/cosmjs/blob/98e91ae5fe699733497befef95204923c93a7373/packages/amino/src/signer.ts#L22-L38 */
 
@@ -1116,9 +1100,9 @@ export function makeAgentWallet(
   };
 }
 
-// Notes
-//
-// [1]: The prefix parameters here are not to be confused with the "didPrefix'
-//      parameter of this "makeWallet" function. The prefixes used in the
-//      Secp256k1HdWallet constructor functions are prefixes for cosmos wallet
-//      addresses while the did prefix is the prefix of the did address.
+// // Notes
+// //
+// // [1]: The prefix parameters here are not to be confused with the "didPrefix'
+// //      parameter of this "makeWallet" function. The prefixes used in the
+// //      Secp256k1HdWallet constructor functions are prefixes for cosmos wallet
+// //      addresses while the did prefix is the prefix of the did address.
